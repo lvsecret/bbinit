b22d4f241ecc2f8f1bc1c93aaf15dae3ae06a1a3
lvsecret <13548672218@163.com>
2018-12-01 11:10:44 +0800

    初始化 路由重定向 导入全局axios 导入全局基础地址 全局过滤器 导入moment.js,element.js插件 完成首页数据渲染

---
+++ src/components/index.vue
@@ -0,0 +1,144 @@
+<template>
+  <div>
+    <div class="section">
+      <div class="location">
+        <span>当前位置：</span>
+        <a href="#/" class="router-link-active">首页</a> &gt;
+        <a href="#/site/goodslist" class="router-link-exact-active router-link-active">购物商城</a>
+      </div>
+    </div>
+    <div class="section">
+      <div class="wrapper">
+        <div class="wrap-box">
+          <div class="left-220" style="margin: 0px;">
+            <div class="banner-nav">
+              <ul>
+                <li v-for="(item,inde) in catelist" :key="item.id">
+                  <h3>
+                    <i class="iconfont icon-arrow-right"></i>
+                    <span>{{item.title}}</span>
+                    <p>
+                      <span v-for="(it,n) in item.subcates " :key="it.id">{{it.title}}&nbsp;</span>
+                    </p>
+                  </h3>
+                  <div class="item-box">
+                    <dl>
+                      <dt>
+                        <a href="/goods/40.html">{{item.title}}</a>
+                      </dt>
+                      <dd>
+                        <a
+                          v-for="(it,n) in item.subcates"
+                          :key="it.id"
+                          href="/goods/43.html"
+                        >{{it.title}}</a>
+                      </dd>
+                    </dl>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!--幻灯片-->
+          <div class="left-705">
+            
+            <div class="banner-img">
+             <el-carousel height="341px">
+              <el-carousel-item v-for="item in sliderlist" :key="item.id">
+                <img :src="item.img_url" alt="">
+              </el-carousel-item>
+            </el-carousel>
+            </div>
+          </div>
+          <!--/幻灯片-->
+          <div class="left-220">
+            <ul v-for="(item,index) in toplist" :key="item.id" class="side-img-list">
+              <li>
+                <div class="img-box">
+                  <label>{{index+1}}</label>
+                  <img :src="item.img_url">
+                </div>
+                <div class="txt-box">
+                  <a href="/goods/show-98.html">{{item.title}}</a>
+                  <span>{{item.add_time | shortTime }}</span>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-for="(item,index) in goodsList" :key="item.level1cateid" class="section">
+      <div class="main-tit">
+        <h2>{{item.catetitle}}</h2>
+        <p>
+          <a v-for="(it,n) in item.level2catelist" :key="it.subcateid" href="/goods/43.html">{{it.subcatetitle}}</a>
+          <a href="/goods/40.html">
+            更多
+            <i>+</i>
+          </a>
+        </p>
+      </div>
+      <div class="wrapper clearfix">
+        <div class="wrap-box">
+          <ul  class="img-list">
+            <li v-for="(it,n) in item.datas" :key="it.artID">
+              <a href="#/site/goodsinfo/87" class>
+                <div class="img-box">
+                  <img
+                    :src="it.img_url"
+                  >
+                </div>
+                <div class="info">
+                  <h3>{{it.artTitle}}</h3>
+                  <p class="price">
+                    <b>{{it.sell_price}}</b>元
+                  </p>
+                  <p>
+                    <strong>库存 {{it.stock_quantity}}</strong>
+                    <span>
+                      市场价：
+                      <s>{{it.market_price}}</s>
+                    </span>
+                  </p>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "index",
+  data: function() {
+    return {
+      catelist: [],
+      sliderlist: [],
+      toplist: [],
+      goodsList:[],
+    };
+  },
+  created() {
+    this.$axios.get(`site/goods/gettopdata/goods`).then(result => {
+    //   console.log(result);
+      this.catelist = result.data.message.catelist;
+      this.sliderlist = result.data.message.sliderlist;
+      this.toplist = result.data.message.toplist;
+    });
+    this.$axios.get(`site/goods/getgoodsgroup`).then(result=>{
+        console.log(result);
+        this.goodsList=result.data.message;
+
+    })
+  }
+};
+</script>
+<style >
+    .banner-img img{
+        width: 100%;
+        height: 100%;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,61 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
+// 导入全局css
+import './assets/site/css/style.css'
+
+//导入全局axios
+//设置到Vue的原型上,那么所有实例化出来的对象和组件都能够共享这个属性
+//一般来说 设置到原型上的属性 vue中会使用$作为前缀原来区分普通的属性
+import axios from 'axios'
+Vue.prototype.$axios=axios;
+
+//导入全局moment.js
+import moment from 'moment'
+
+//导入element.js
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+//全局过滤器
+Vue.filter('shortTime', value=> {
+//  console.log(moment(value).format('YYYY(⊙o⊙)MM(⊙o⊙)MDD'));
+  return moment(value).format('YYYY(⊙o⊙)MM(⊙o⊙)MDD');
+})
+
+
+
+//使用axios的方式设置基础地址
+axios.defaults.baseURL = 'http://111.230.232.110:8899/';
+
+//0.导入路由
+//如果在一个模块化工程中使用它，必须要通过 Vue.use() 明确地安装路由功能：
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+//1.定义(路由)组件
+import index from './components/index.vue'
+
+//2.定义路由规则
+let routes= [
+  {
+    path:'/',
+    //重定向到首页
+    redirect:'/index'
+  },
+  {
+    path:'/index',
+    component:index
+  }
+]
+//3.创建 router 实例，然后传 `routes` 配置
+let router=new VueRouter({
+  routes// (缩写) 相当于 routes: routes
+})
 new Vue({
   render: h => h(App),
+  //4.传入路由对象
+  router
 }).$mount('#app')
